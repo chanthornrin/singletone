f4a27867d8917a73b7577c94da9d35602e17dbd5
chanthornrin <81553923+chanthornrin@users.noreply.github.com>
2021-07-31 16:59:22 +0700

    Add files via upload

---
+++ Coffee.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleTon
+{
+    class Coffee
+    {
+        private Coffee() { }
+
+        private static Coffee Instance = null;
+        public static Coffee GetInstance
+        {
+            get
+            {
+                if (Instance == null)
+                {
+                    Instance = new Coffee();
+                }
+                return Instance;
+            }
+
+        }
+
+        public void Quotes(String qaulity, String price, String reqirment)
+        {
+            Console.WriteLine("Quality:" + qaulity + "\n Prices:" + price +
+
+   "\nRequirments:" + reqirment);
+        }
+
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace singleTon
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Coffee cus1 = Coffee.GetInstance;
+
+            cus1.Quotes("good","good","good");
+
+            Coffee cus2 = Coffee.GetInstance;
+            cus2.Quotes("good", "good", "good"); 
+            Console.Read();
+        }
+    }
+}
